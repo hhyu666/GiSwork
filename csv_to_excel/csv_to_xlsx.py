aaf86e0d9526f4a0e4cad937dd7bea4c3d3170ce
hhyu666 <63549780+hhyu666@users.noreply.github.com>
2023-01-16 10:54:53 +0800

    1.16

--- csv_to_excel/csv_to_xlsx.py
+++ csv_to_excel/csv_to_xlsx.py
@@ -1,20 +1,29 @@
 import os
+import sys
+
 import pandas as pd
 def csv_to_xlsx(data_path):
+    print('Start!'+data_path)
     for dirpath,dirname,filenames in os.walk(data_path): #os.walk()遍历文件
+        print(dirpath)
         for fname in filenames:
             file_name=os.path.join(dirpath,fname)  #os.path.join()合并路径
+            print(file_name)
             if file_name.endswith('.csv'):
                 df=pd.read_csv(file_name) 
                 floder,name=os.path.splitext(file_name) #分割后缀
                 df.to_excel(f"{floder}.xlsx",index=False)
-                print(f'{file_name}') 
+                print(f'{file_name}'+'finished.') 
                 print('-----------')
-                os.remove(file_name)
+                # os.remove(file_name)
     print('Finished...')
 
 
-data_path=r"F:\GISwork"
-csv_to_xlsx(data_path) 
+
+if __name__ == '__main__':
+    data_path = sys.argv[1]
+    csv_to_xlsx(data_path)
+
+
 
 
