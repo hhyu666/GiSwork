23a5d7b5791944ce618d19e0d82e70249b2e2060
hhyu666 <63549780+hhyu666@users.noreply.github.com>
2023-01-16 14:27:42 +0800

    1-16

---
+++ csv_to_excel/xlsx_to_csv.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+import pandas as pd
+def xlsx_to_csv(data_path):
+    print('Start!'+data_path)
+    for dirpath,dirname,filenames in os.walk(data_path): #os.walk()遍历文件
+        print(dirpath)
+        for fname in filenames:
+            file_name=os.path.join(dirpath,fname)  #os.path.join()合并路径
+            print(file_name)
+            if file_name.endswith('.xlsx'):
+                df=pd.read_excel(file_name) 
+                floder,name=os.path.splitext(file_name) #分割后缀
+                df.to_csv(f"{floder}.csv",index=False)
+                print(f'{file_name}'+'finished.') 
+                print('-----------')
+                # os.remove(file_name)
+    print('Finished...')
+
+
+
+if __name__ == '__main__':
+    data_path = sys.argv[1]
+    xlsx_to_csv(data_path)
